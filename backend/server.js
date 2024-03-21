2b5cd0b301519dbfb410b2a751a6067a5f9abb91
Kingcedru <byishimocedrick@gmail.com>
2024-03-21 19:05:14 +0200

    created dummy routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", )
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.post("/users", (req, res) => {
+  res.json({
+    name: req.body.name,
+    email: req.body.email,
+    paassword: req.body.password,
+  });
+});
+
+app.get("/users/:id", (req, res) => {
+    let id = req.params.id;
+    res.json({id:id})
+})
 
 app.listen(5000);
