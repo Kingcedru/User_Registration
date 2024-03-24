9d035a580b2b9c3c79f797c027f47b25eb8a8955
Kingcedru <byishimocedrick@gmail.com>
2024-03-24 12:58:02 +0200

    added hashing and tokens

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 mongoose
@@ -13,7 +15,19 @@ const Users = require("./model/Users");
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
+authCheck = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, "success");
+    req.userData = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: "No allowed to access" });
+  }
+};
+
+app.get("/users", authCheck, (req, res) => {
   Users.find()
     .then((response) => {
       res.send(response);
@@ -25,21 +39,55 @@ app.get("/users", (req, res) => {
 
 app.post("/users", async (req, res) => {
   try {
-    let user = await new Users({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    user
-      .save()
-      .then((response) => {
-        res.status(201).send(user);
-      })
-      .catch((err) => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
         console.log(err);
-      });
+      } else {
+        let user = new Users({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash,
+        });
+        user
+          .save()
+          .then((response) => {
+            res.status(201).send(user);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
   } catch (err) {
     console.log(err);
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  try {
+    const user = await Users.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json({ message: "Auth failed" });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (isMatch) {
+      const token = jwt.sign(
+        {
+          email: user.email,
+          password: user.password,
+        },
+        "success",
+        {
+          expiresIn: "1h",
+        }
+      );
+      return res.json({ message: "Auth successful", token: token });
+    } else {
+      res.status(401).json({ message: "Auth failed" });
+    }
+  } catch (err) {
+    console.error("Error during login:", err);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
@@ -70,7 +118,7 @@ app.delete("/users/:id", (req, res) => {
 });
 
 app.patch("/users/:id", (req, res) => {
-  Users.findByIdAndUpdate(req.params.id, { name: req.body.name },{new:true})
+  Users.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
     .then((result) => {
       res.json(result);
     })
